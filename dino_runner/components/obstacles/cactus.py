b3e39566ec53c9ca0c6dcc04c63044ac0ebe7973
G-king27 <119895094+G-king27@users.noreply.github.com>
2022-12-07 11:03:15 -0400

    Tarea 2 Dinosaur Game

---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,20 @@
+from pygame import Surface
+from random import randint
+
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+
+class Cactus(Obstacle):
+    Y_POS_LARGE_CACTUS = 300
+
+    def __init__(self, images: list[Surface], type):
+        super(). __init__(images, randint(0, 2))
+        self.rect.y = self.Y_POS_LARGE_CACTUS
+
+
+class SmallCactus(Obstacle):
+    Y_POS_SMALL_CACTUS = 325
+    
+    def __init__(self, images: list[Surface], type):
+        super(). __init__(images, randint(0, 2))
+        self.rect.y = self.Y_POS_SMALL_CACTUS
